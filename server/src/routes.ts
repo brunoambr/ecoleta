625892ebef870cd54dbf84d65f09379845dd2b23
Bruno Ambrozio <brunoc.ambrozio@gmail.com>
2020-06-13 18:12:09 -0300

    chore: Adicionar upload de imagens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,28 @@
 import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import ItemsController from './controllers/ItemsController';
 import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
+const upload = multer(multerConfig);
+
 const itemsController = new ItemsController();
 const pointsController = new PointsController();
 
+// #region GET
+
 routes.get('/items', itemsController.index);
-routes.get('/points', pointsController.index)
-routes.get('/points/:id', pointsController.show)
-routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+// #endregion
+
+// #region POST
+
+routes.post('/points', upload.single('image'), pointsController.create); // upload.single('image') faz o upload do campo image na pasta uploads, dadas as configurações do mutler
+
+// #endregion
 
 export default routes;
